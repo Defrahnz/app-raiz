5f9b4dc3416aeede56162db40834d40db81376d8
Daniel Valdivia <dvaldivia98@hotmail.com>
2025-07-28 23:56:40 -0600

    Cargamos los archivos .env en el proyecto principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 // Importa el nuevo contenedor de navegación
 import 'main_navigator_cont.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
